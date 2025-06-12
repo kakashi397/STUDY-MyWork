31e43d9740fc05d9e3801311bb680e8feb138415
Takashi Ota <gmkzkks1011@gmail.com>
2025-06-12 18:39:19 +0900

    ハンバーガーボタンのアニメーションを実装中

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* ハンバーガーボタン */
+const hamburgerBtn = document.querySelector('js-hamburger');
+
+hamburgerBtn.addEventListener('click', () => {
+  hamburgerBtn.classList.toggle('is-active');
+});
+
+/* スライドメニュー */
